7b70f5e3750fc73dd7a632413f83949e63aaf27b
CreggEgg <eggcregg@gmail.com>
2023-09-08 12:35:01 -0400

    sdfd

---
+++ src/routes/butter-game/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Butter game</h1>
+<p>
+  As many frc teams surely have, as a team we've theorized about possible future
+  games. Unlike other teams, we actually have good ideas because we are so cool
+  and smart. If you'd like to learn more about the butter game concept (*wink
+  wink* Chris Moore CEO of FIRST (for inspiration and recognition of science and
+  tech (technology))) you can email our coach <a
+    href="mschoifet@friendscentral.org">Matthew Schoifet!</a
+  >
+</p>
---
+++ src/routes/past-robots/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Past robots</h1>
+
+<ul>
+  <li>
+    <hgroup>
+      <h2>Namazu</h2>
+      <p>2023 season</p>
+    </hgroup>
+    <figure>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/MWyBbpCCDfY?si=MqsH5cfjrtsv5av9"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      />
+      <figcaption>Our robot reveal video</figcaption>
+    </figure>
+    <p>
+      In 2023, our rookie year, we created Namazu. Named after a giant catfish
+      responsible for earthquakes in Japanese mythology, Namazu shook up the
+      competition with its quaking capabilities. This season we made it all the
+      way to districts but ended up short on points to move on to worlds.
+      😭🥺🥲😟👉👈
+    </p>
+  </li>
+</ul>
