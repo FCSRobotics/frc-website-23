b18577533ab3b46f6a44c507de9534f7e32028a8
CreggEgg <eggcregg@gmail.com>
2023-09-09 13:22:43 -0400

    stuff

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,32 @@
+<h1>Follow us</h1>
+<section>
+	<h2>Social Media</h2>
+	<div id="social-icons">
+		<a target="_blank" href="https://www.instagram.com/earthquakers9094/"
+			><img src="/instagram.png" alt="instagram" /></a
+		>
+		<a target="_blank" href="https://www.youtube.com/@EarthquakersRobotics9094"
+			><img src="/youtube.png" alt="youtube" /></a
+		>
+	</div>
+</section>
+
+<style>
+	#social-icons {
+		display: grid;
+		place-items: center;
+		grid-template-columns: 1fr 1fr;
+		gap: 20px;
+		width: max-content;
+		margin: auto;
+	}
+
+	section {
+		width: min-content;
+		margin: auto;
+	}
+
+	img {
+		width: 5vw;
+	}
+</style>
