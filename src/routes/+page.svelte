d441d0ea891341582a65460e4059436bf7144991
FCSRobotics <mschoifet@friendscentral.org>
2023-09-11 13:03:07 -0400

    changed bottom margin of h1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,18 +5,20 @@
 <h1>Earthquakers 9094</h1>
 <div role="doc-subtitle">Subheading</div>
 <p>
-  &nbsp; &nbsp; &nbsp;The Earthquakers, established in the fall of 2022, are
-  the&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;robotics team representing Friends'
-  Central School. Friends' Central School is a Quaker, community-based school.
-  The Quaker values of simplicity, peace, equality, and integrity guide our
-  interactions with ourselves and our community.&nbsp;In founding a FIRST
-  Robotics team, we incorporate these ideas into FIRST’s mission of inspiring
-  students to pursue careers in science and technology. We hope that by creating
-  an accessible, collaborative robotics program, we can develop students with
-  the skill sets to affect change in the world around them.
+  The Earthquakers, established in the fall of 2022, are the robotics team
+  representing Friends' Central School. Friends' Central School is a Quaker,
+  community-based school. The Quaker values of simplicity, peace, equality, and
+  integrity guide our interactions with ourselves and our community. In founding
+  a FIRST Robotics team, we incorporate these ideas into FIRST’s mission of
+  inspiring students to pursue careers in science and technology. We hope that
+  by creating an accessible, collaborative robotics program, we can develop
+  students with the skill sets to affect change in the world around them.
 </p>
 
 <style>
+  h1 {
+    margin-bottom: 0px;
+  }
   [role="doc-subtitle"] {
     color: #c2c2c2;
   }
