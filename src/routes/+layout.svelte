ca01509bcd5285aecee4424f23eb10ac8f05fd32
FCSRobotics <mschoifet@friendscentral.org>
2023-09-11 11:58:15 -0400

    something

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,55 +1,57 @@
 <nav>
-	<a href="/">Home</a><a href="/about">About us</a><a href="/past-robots"
-		>Past robots</a
-	>
-	<a href="/contact">Follow us</a>
+  <a href="/">Home</a><a href="/about">About us</a><a href="/past-robots"
+    >Past robots</a
+  >
+  <a href="/contact">Follow us</a>
+  <a href="/our-community">Our Community</a>
+  <a href="/what-is-first">What is FIRST?</a>
 </nav>
 
 <main>
-	<slot />
+  <slot />
 </main>
 
 <link rel="stylesheet" href="/global.css" />
 
 <style>
-	nav {
-		background-color: var(--primary);
-		width: 100%;
-		height: 5%;
-		font-size: 1.5rem;
-		/* display: flex;
+  nav {
+    background-color: var(--primary);
+    width: 100%;
+    height: 5%;
+    font-size: 1.5rem;
+    /* display: flex;
 		justify-content: center;
 		align-items: center; */
-		display: grid;
-		place-items: center;
-		grid-template-rows: 1fr;
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		position: fixed;
-		top: 0px;
-		left: 0px;
-	}
-	main {
-		margin-top: 2%;
-		padding: 2rem;
-		text-align: center;
-	}
-	html {
-		background-color: var(--background);
-	}
+    display: grid;
+    place-items: center;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    position: fixed;
+    top: 0px;
+    left: 0px;
+  }
+  main {
+    margin-top: 2%;
+    padding: 2rem;
+    text-align: center;
+  }
+  html {
+    background-color: var(--background);
+  }
 
-	nav > a {
-		color: white;
-		background-color: var(--primary);
-		height: 100%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		width: 100%;
-		transition: background-color 0.25s;
-		text-decoration: none;
-	}
-	nav > a:hover,
-	nav > a#selected {
-		background-color: var(--accent);
-	}
+  nav > a {
+    color: white;
+    background-color: var(--primary);
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    transition: background-color 0.25s;
+    text-decoration: none;
+  }
+  nav > a:hover,
+  nav > a#selected {
+    background-color: var(--accent);
+  }
 </style>
