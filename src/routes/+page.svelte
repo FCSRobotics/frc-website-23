e0b10e9f6c025478c948c0724d789113dcfc93d3
FCSRobotics <mschoifet@friendscentral.org>
2023-09-11 12:59:17 -0400

    made some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,23 @@
 <svelte:head>
-	<title>Earthquakers 9094</title>
+  <title>Earthquakers 9094</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
+<h1>Earthquakers 9094</h1>
+<div role="doc-subtitle">Subheading</div>
 <p>
-	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  &nbsp; &nbsp; &nbsp;The Earthquakers, established in the fall of 2022, are
+  the&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;robotics team representing Friends'
+  Central School. Friends' Central School is a Quaker, community-based school.
+  The Quaker values of simplicity, peace, equality, and integrity guide our
+  interactions with ourselves and our community.&nbsp;In founding a FIRST
+  Robotics team, we incorporate these ideas into FIRST’s mission of inspiring
+  students to pursue careers in science and technology. We hope that by creating
+  an accessible, collaborative robotics program, we can develop students with
+  the skill sets to affect change in the world around them.
 </p>
+
+<style>
+  [role="doc-subtitle"] {
+    color: #c2c2c2;
+  }
+</style>
