2fc9a9f203508cbf79f6e1ab13502d2b0a3231e5
FCSRobotics <mschoifet@friendscentral.org>
2023-09-13 18:03:04 -0400

    stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,98 +1,53 @@
-<nav>
-  <a href="/">Home</a><a href="/past-robots">Past robots</a>
-  <a href="/our-community">Our Community</a>
-  <a href="/what-is-first">What is FIRST?</a>
-</nav>
-
-<main>
-  <slot />
-</main>
-
-<!-- <footer>
-  <h1>Social Media</h1>
-  <div id="social-icons">
-    <a target="_blank" href="https://www.instagram.com/earthquakers9094/"
-      ><img src="/instagram.png" alt="instagram" /></a
-    >
-    <a target="_blank" href="https://www.youtube.com/@EarthquakersRobotics9094"
-      ><img src="/youtube.png" alt="youtube" /></a
-    >
-  </div>
-</footer> -->
-
 <link rel="stylesheet" href="/global.css" />
+
+<div id="container">
+  <nav>
+    <a href="/"><img id="logo" src="/logo.png" alt="Earthquakers 9094" /></a><a
+      href="/first">What is FIRST?</a
+    ><a href="/about">About our team</a><a href="/seasons">Past seasons</a><a
+      href="/sponsors">Sponsors</a
+    ><a href="/contact">Contact us</a>
+  </nav>
+
+  <main>
+    <slot />
+  </main>
+</div>
 
 <style>
   nav {
     background-color: var(--primary);
-    width: 100%;
-    height: 5%;
-    font-size: 1.5rem;
-    /* display: flex;
-		justify-content: center;
-		align-items: center; */
-    display: grid;
-    place-items: center;
-    grid-template-rows: 1fr;
-    grid-template-columns: 1fr 1fr 1fr 1fr;
-    position: fixed;
+    height: 15vh;
+    position: sticky;
     top: 0px;
     left: 0px;
-    z-index: 9999;
-  }
-  main {
-    margin: 0px;
-    margin-top: 2%;
-    margin-bottom: 10rem;
-    text-align: center;
-  }
-
-  html {
-    background-color: var(--background);
-  }
-
-  nav > a {
+    right: 0px;
+    z-index: 999;
     color: white;
-    background-color: var(--primary);
-    height: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 100%;
-    transition: background-color 0.25s;
     text-decoration: none;
-  }
-  nav > a:hover,
-  nav > a#selected {
-    background-color: var(--accent);
-  }
-
-  footer {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    background-color: var(--accent);
-    padding-bottom: 7rem;
-  }
-
-  #social-icons {
-    width: 40%;
     display: grid;
     place-items: center;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr repeat(5, 1fr);
   }
 
-  #social-icons > a {
+  #container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  main {
+    height: 100%;
+  }
+  #logo {
     width: 100%;
+  }
+  a {
+    color: white;
+    font-size: 1.5rem;
+    width: 100%;
+    height: 100%;
     display: flex;
     align-items: center;
     justify-content: center;
-  }
-
-  a > img {
-    width: 50%;
-    margin: auto;
   }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,67 +1,21 @@
-<svelte:head>
-  <title>Earthquakers 9094</title>
-</svelte:head>
-
 <header>
-  <div id="blur">
-    <img src="/logo.png" alt="Earthquakers 9094" />
-    <!-- <h1>Earthquakers 9094</h1> -->
-    <div role="doc-subtitle">Robotics team of Friends' Central School</div>
-  </div>
+  <img id="logo" src="/logo-high.png" alt="Earthquakers 9094" />
 </header>
-<p>
-  The Earthquakers, established in the fall of 2022, are the robotics team
-  representing Friends' Central School. Friends' Central School is a Quaker,
-  community-based school. The Quaker values of simplicity, peace, equality, and
-  integrity guide our interactions with ourselves and our community. In founding
-  a FIRST Robotics team, we incorporate these ideas into FIRST’s mission of
-  inspiring students to pursue careers in science and technology. We hope that
-  by creating an accessible, collaborative robotics program, we can develop
-  students with the skill sets to affect change in the world around them.
-</p>
 
 <style>
-  h1 {
-    margin: 0px;
-  }
-  [role="doc-subtitle"] {
-    /* color: #c2c2c2; */
-    font-size: 2rem;
-  }
-
-  p {
-    font-size: 1.3rem;
-    width: 60%;
-    margin: auto;
+  #logo {
+    width: 100%;
   }
 
   header {
-    color: white;
-    width: 100%;
-
-    text-align: center;
-    padding-top: 10rem;
-    padding-bottom: 10rem;
-    background: url("/hero.png");
+    background-image: url("/hero.png");
     background-size: cover;
     background-repeat: no-repeat;
-    background-position: center 75%;
-    /* filter: blur(8px);
-    -webkit-filter: blur(8px); */
-    margin-bottom: 5vh;
-  }
-
-  #blur {
+    height: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    width: 100%;
-    height: 100%;
-
-    backdrop-filter: blur(8px) brightness(60%);
-    -webkit-backdrop-filter: blur(8px) brightness(60%);
-    /*sdfdf*/
-    padding-top: 5rem;
+    text-align: center;
   }
 </style>
