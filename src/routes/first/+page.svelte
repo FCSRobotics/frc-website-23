5340c55d93e36ac63313660da6901a256a37dc3e
FCSRobotics <mschoifet@friendscentral.org>
2023-09-14 09:13:47 -0400

    gjsdklfjlkd

---
+++ src/routes/first/+page.svelte
@@ -0,0 +1,9 @@
+<h1>What is FIRST?</h1>
+<p>
+  As our lord and savior, Dean "hits table for emphasis" Kamen once said, "It's
+  not about building robots, its about ramming the robots into each other." When
+  we heard this inspirational, life changing, impactful quote for the first
+  time, we knew what we were capable of. We might not be able to create a robot
+  to rival our opponents, but we can sure hit them so hard that their battery
+  falls out and they fall over.
+</p>
