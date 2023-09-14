6a49065cf0947a8bf78b5fb4edad9e701e2f05cb
CreggEgg <eggcregg@gmail.com>
2023-09-13 23:10:35 -0400

    new stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,74 @@
 <link rel="stylesheet" href="/global.css" />
 
 <div id="container">
-  <nav>
-    <a href="/"><img id="logo" src="/logo.png" alt="Earthquakers 9094" /></a><a
-      href="/first">What is FIRST?</a
-    ><a href="/about">About our team</a><a href="/seasons">Past seasons</a><a
-      href="/sponsors">Sponsors</a
-    ><a href="/contact">Contact us</a>
-  </nav>
+	<nav>
+		<a id="with-logo" href="/"
+			><img id="logo" src="/logo.png" alt="Earthquakers 9094" /></a
+		><a href="/first">What is FIRST?</a><a href="/about">About our team</a><a
+			href="/seasons">Past seasons</a
+		><a href="/sponsors">Sponsors</a><a href="/contact">Contact us</a>
+	</nav>
 
-  <main>
-    <slot />
-  </main>
+	<main>
+		<slot />
+	</main>
 </div>
 
 <style>
-  nav {
-    background-color: var(--primary);
-    height: 15vh;
-    position: sticky;
-    top: 0px;
-    left: 0px;
-    right: 0px;
-    z-index: 999;
-    color: white;
-    text-decoration: none;
-    display: grid;
-    place-items: center;
-    grid-template-columns: 1fr repeat(5, 1fr);
-  }
+	nav {
+		background: linear-gradient(0.25turn, var(--primary), var(--accent));
+		height: 15vh;
+		position: sticky;
+		top: 0px;
+		left: 0px;
+		right: 0px;
+		z-index: 999;
+		color: white;
+		text-decoration: none;
+		display: grid;
+		place-items: center;
+		grid-template-columns: 1fr repeat(5, 1fr);
+	}
 
-  #container {
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-  main {
-    height: 100%;
-  }
-  #logo {
-    width: 100%;
-  }
-  a {
-    color: white;
-    font-size: 1.5rem;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  }
+	#container {
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+	}
+	main {
+		height: 100%;
+	}
+	#logo {
+		width: 100%;
+	}
+	nav > a {
+		color: white;
+		font-size: 1.3vw;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-decoration: none;
+		scale: 1;
+		transition: scale 0.5s;
+	}
+
+	a:hover:not(:nth-child(1)) {
+		/* font-size: 2rem; */
+		scale: 1.5;
+	}
+
+	@media (max-width: 600px) {
+		nav > a {
+			display: none;
+		}
+		nav > a#with-logo {
+			display: flex;
+		}
+
+		nav {
+			grid-template-columns: 1fr 1fr;
+		}
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,41 @@
 <header>
-  <img id="logo" src="/logo-high.png" alt="Earthquakers 9094" />
+	<div id="headings">
+		<img id="logo" src="/logo-high.png" alt="Earthquakers 9094" />
+		<h2>Friends' Central School Robotics</h2>
+	</div>
+	<img src="" alt="" />
 </header>
 
 <style>
-  #logo {
-    width: 100%;
-  }
+	#logo {
+		width: max(60vw, 100%);
+	}
 
-  header {
-    background-image: url("/hero.png");
-    background-size: cover;
-    background-repeat: no-repeat;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-  }
+	header {
+		background-image: url("/hero.png");
+		background-size: cover;
+		background-repeat: no-repeat;
+		height: 100%;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		color: white;
+	}
+
+	h2 {
+		font-size: max(2vw, 0.5rem);
+	}
+
+	#headings {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		width: 60%;
+		padding: 50px;
+		border-radius: 50px;
+		backdrop-filter: blur(8px) brightness(50%);
+	}
 </style>
