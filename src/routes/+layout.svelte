7eede55c93baba2c58b436ac439b6d7057e28329
FCSRobotics <mschoifet@friendscentral.org>
2023-09-14 09:02:05 -0400

    more nav work

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,74 +1,131 @@
 <link rel="stylesheet" href="/global.css" />
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0"
+/>
 
 <div id="container">
-	<nav>
-		<a id="with-logo" href="/"
-			><img id="logo" src="/logo.png" alt="Earthquakers 9094" /></a
-		><a href="/first">What is FIRST?</a><a href="/about">About our team</a><a
-			href="/seasons">Past seasons</a
-		><a href="/sponsors">Sponsors</a><a href="/contact">Contact us</a>
-	</nav>
+  <nav>
+    <a id="with-logo" href="/"
+      ><img id="logo" src="/logo.png" alt="Earthquakers 9094" /></a
+    ><a href="/first">What is FIRST?</a><a href="/about">About our team</a><a
+      href="/seasons">Past seasons</a
+    ><a href="/sponsors">Sponsors</a><a href="/contact">Contact us</a>
+    <span id="spacer" />
+    <div id="dropdown">
+      <button id="mobile-dropdown"
+        ><span class="material-symbols-outlined"> menu </span></button
+      >
+      <div id="dropdown-content">
+        <a href="/first">What is FIRST?</a><a href="/about">About our team</a><a
+          href="/seasons">Past seasons</a
+        ><a href="/sponsors">Sponsors</a><a href="/contact">Contact us</a>
+      </div>
+    </div>
+  </nav>
 
-	<main>
-		<slot />
-	</main>
+  <main>
+    <slot />
+  </main>
 </div>
 
 <style>
-	nav {
-		background: linear-gradient(0.25turn, var(--primary), var(--accent));
-		height: 15vh;
-		position: sticky;
-		top: 0px;
-		left: 0px;
-		right: 0px;
-		z-index: 999;
-		color: white;
-		text-decoration: none;
-		display: grid;
-		place-items: center;
-		grid-template-columns: 1fr repeat(5, 1fr);
-	}
+  nav {
+    background: linear-gradient(0.25turn, var(--primary), var(--accent));
+    height: 15vh;
+    position: sticky;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    z-index: 999;
+    color: white;
+    text-decoration: none;
+    display: grid;
+    place-items: center;
+    grid-template-columns: 1fr repeat(5, 1fr);
+  }
 
-	#container {
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-	}
-	main {
-		height: 100%;
-	}
-	#logo {
-		width: 100%;
-	}
-	nav > a {
-		color: white;
-		font-size: 1.3vw;
-		width: 100%;
-		height: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		text-decoration: none;
-		scale: 1;
-		transition: scale 0.5s;
-	}
+  #container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  main {
+    height: 100%;
+  }
+  #logo {
+    width: 100%;
+  }
+  nav > a {
+    color: white;
+    font-size: 1.3vw;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    text-decoration: none;
+    scale: 1;
+    transition: scale 0.5s;
+  }
 
-	a:hover:not(:nth-child(1)) {
-		/* font-size: 2rem; */
-		scale: 1.5;
-	}
+  nav > a:hover:not(:nth-child(1)) {
+    /* font-size: 2rem; */
+    scale: 1.5;
+  }
 
-	@media (max-width: 600px) {
-		nav > a {
-			display: none;
-		}
-		nav > a#with-logo {
-			display: flex;
-		}
+  #dropdown {
+    display: none;
+  }
 
-		nav {
-			grid-template-columns: 1fr 1fr;
-		}
-	}
+  #spacer {
+    display: none;
+  }
+
+  #dropdown-content {
+    display: none;
+    position: absolute;
+    right: 0px;
+    padding: 2vw;
+    background-color: var(--primary);
+    z-index: 9999;
+    border-radius: 15px;
+  }
+
+  #dropdown-content > a {
+    color: white;
+  }
+
+  @media (max-width: 1000px) {
+    nav > a {
+      display: none;
+    }
+    nav > a#with-logo {
+      display: flex;
+    }
+
+    #spacer {
+      display: block;
+    }
+
+    nav {
+      grid-template-columns: 2fr 5fr 1fr;
+    }
+
+    #dropdown {
+      display: block;
+    }
+
+    #mobile-dropdown {
+      color: white;
+      background: none;
+      border: none;
+    }
+
+    #dropdown:hover > #dropdown-content {
+      display: flex;
+      flex-direction: column;
+      gap: 2vw;
+    }
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,41 +1,51 @@
 <header>
-	<div id="headings">
-		<img id="logo" src="/logo-high.png" alt="Earthquakers 9094" />
-		<h2>Friends' Central School Robotics</h2>
-	</div>
-	<img src="" alt="" />
+  <div id="headings">
+    <img id="logo" src="/logo-high.png" alt="Earthquakers 9094" />
+    <h2>Friends' Central School Robotics</h2>
+  </div>
+  <img src="" alt="" />
 </header>
 
 <style>
-	#logo {
-		width: max(60vw, 100%);
-	}
+  #logo {
+    width: max(60vw, 80%);
+    height: auto;
+  }
 
-	header {
-		background-image: url("/hero.png");
-		background-size: cover;
-		background-repeat: no-repeat;
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		text-align: center;
-		color: white;
-	}
+  header {
+    background-image: url("/team.jpeg");
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center 70%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    color: white;
+  }
 
-	h2 {
-		font-size: max(2vw, 0.5rem);
-	}
+  h2 {
+    font-size: max(2vw, 1rem);
+  }
 
-	#headings {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
-		width: 60%;
-		padding: 50px;
-		border-radius: 50px;
-		backdrop-filter: blur(8px) brightness(50%);
-	}
+  #headings {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    width: 60%;
+    padding: 50px;
+    border-radius: 50px;
+    backdrop-filter: blur(8px) brightness(50%);
+  }
+
+  @media (max-width: 600px) {
+    #logo {
+      content: url("/logo-vertical.png");
+      height: 50vh;
+      width: auto;
+    }
+  }
 </style>
