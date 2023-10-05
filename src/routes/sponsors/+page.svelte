38cf4d8cfc14caa75d275e3d736109df99875c07
FCSRobotics <mschoifet@friendscentral.org>
2023-10-05 10:32:18 -0400

    changes

--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -1,22 +1,40 @@
 <h1>Sponsors!</h1>
+<h2>
+  The Earthquakers would like to thank all our sponsors for their generous
+  support.
+</h2>
 <div class="sponsors-grid">
   <img src="/fcs.png" alt="FCS" />
   <img src="/comcast.png" alt="Comcast" />
 </div>
 
+<h2>How Can You Help?</h2>
+<p>
+  Financial support goes a long way in supporting the team to ensure all
+  logistical needs are met while we have access to the best training and
+  materials available. In return we would love to feature your company's logo on
+  our robot, website, our competition pit, and give your company a shoutout
+  during actual matches. We attend five regional competitions across Eastern
+  Pennsylvania and New Jersey throughout the year giving a chance for your
+  company’s information to be promoted to hundreds of families attending these
+  events. If your organization is interested in sponsoring the Earthquakers,
+  please reach out to mschoifet@friendscentral.org
+</p>
+
 <style>
   .sponsors-grid {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    width: 80%;
+    width: 50%;
+
     margin: auto;
-    gap: 4vw;
+
     justify-items: center;
     align-items: center;
   }
 
   img {
-    width: 30%;
+    width: 10vw;
 
     border-radius: 30px;
   }
