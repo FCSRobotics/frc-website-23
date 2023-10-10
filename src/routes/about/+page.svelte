65d84f2bd2a32028c1ba26d1f2093cd5a7d69ab1
FCSRobotics <mschoifet@friendscentral.org>
2023-10-10 09:42:58 -0400

    fixed oliver

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,35 +1,37 @@
 <div id="content">
-	<h1>About</h1>
-	<h2>Earthquakers Mission</h2>
-	<p>
-		The Earthquaker’s strive to integrate FIRST’s mission of building future
-		leaders in science and technology with Friends’ Central School’s mission to
-		create students who “aim higher” and “go further”. Guided by the school’s
-		Quaker values of peace, equality, integrity, and community, we create a
-		collaborative environment where all students voices, no matter their
-		background, are represented in our robotics community. The Earthquakers aim
-		to create confident, knowledgeable, and compassionate young adults, who are
-		commited to giving back to the community around them through
-	</p>
-	<h2>About the Center for Innovation and Design</h2>
-	<p>
-		Envisioned by our current head of school Beth Johnson, The Center for
-		Innovation and Design (CID) is an exciting architectural project that will
-		transform the Rex Gymnasium on Friends’ Central School’s upper campus into a
-		state-of-the-art makerspace. The first phase of the CID began in the fall of
-		2022 and the final construction of the CID will be completed by the start of
-		the 2024-25 school year. The Earthquakers were created by a group of
-		enthusiastic students and mentors as part of the mission of the CID to
-		promote design thinking, incorporate advanced technology, and integrate
-		hands-on making into the Friends’ Central curriculum so that we can remain a
-		leader in 21st century education.
-	</p>
+  <h1>About</h1>
+  <h2>Earthquakers Mission</h2>
+  <p>
+    The Earthquaker’s strive to integrate FIRST’s mission of building future
+    leaders in science and technology with Friends’ Central School’s mission to
+    create students who “aim higher” and “go further”. Guided by the school’s
+    Quaker values of peace, equality, integrity, and community, we create a
+    collaborative environment where all students voices, no matter their
+    background, are represented in our robotics community. The Earthquakers aim
+    to create confident, knowledgeable, and compassionate young adults, who are
+    commited to giving back to the community around them through
+  </p>
+  <div id="cid-text">
+    <h2>About the Center for Innovation and Design</h2>
+    <p>
+      Envisioned by our current head of school Beth Johnson, The Center for
+      Innovation and Design (CID) is an exciting architectural project that will
+      transform the Rex Gymnasium on Friends’ Central School’s upper campus into
+      a state-of-the-art makerspace. The first phase of the CID began in the
+      fall of 2022 and the final construction of the CID will be completed by
+      the start of the 2024-25 school year. The Earthquakers were created by a
+      group of enthusiastic students and mentors as part of the mission of the
+      CID to promote design thinking, incorporate advanced technology, and
+      integrate hands-on making into the Friends’ Central curriculum so that we
+      can remain a leader in 21st century education.
+    </p>
+  </div>
 </div>
 
 <style>
-	#content {
-		width: 60%;
-		height: 100%;
-		margin: auto;
-	}
+  #content {
+    width: 60%;
+    height: 100%;
+    margin: auto;
+  }
 </style>
